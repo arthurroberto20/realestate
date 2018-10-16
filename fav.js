20844d109b8aa6568b119a01703af9a7642f63ad
arthurroberto20 <arthurroberto20@gmail.com>
2018-10-16 18:03:23 -0300

    lol
    lol

--- fav.js
+++ fav.js
@@ -112,4 +112,28 @@ if (this.className == 'imoveisfav active') {
 
 	}
 	
-);
+)
+;
+$('#add').click( function() {
+	var Description = $('#description').val();
+   if($("#description").val() == '') {
+	 $('#alert').html("<strong>Warning!</strong> You left the to-do empty");
+	 $('#alert').fadeIn().delay(1000).fadeOut();
+	 return false;
+	}
+	$('#todos').prepend("<li><input id='check' name='check' type='checkbox'/>" + Description + "</li>");
+	$('#form')[0].reset();
+	var todos = $('#todos').html();
+	localStorage.setItem('todos', todos);
+	return false;
+ });
+ 
+ if(localStorage.getItem('todos')) {
+ $('#todos').html(localStorage.getItem('todos'));
+ }
+ 
+ $('#clear').click( function() {
+ window.localStorage.clear();
+ location.reload();
+ return false;
+ });
